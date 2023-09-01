2eabb7612ef47108f21982133f5c58f806a8d865
“ihsanunot” <ihsanunot@gmail.com>
2023-09-02 00:26:52 +0700

    routing

--- server.js
+++ server.js
@@ -1,52 +1,47 @@
 const http = require('http');
-
-const requestListerner = (request,response) => {
-
-    response.setHeader('Content-Type','text/html');
+ 
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
-
-    // ambil nilai dari method request
-    const { method } = request;
-
-    if(method === 'GET'){
-        response.end('<h1>Hallo HTTP Server!</h1>');
-    }
-
-    if(method === 'POST') {
-    let body = [];
+ 
+    const { method, url } = request;
+ 
+    if(url === '/') {
+        if(method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>');
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    } else if(url === '/about') {
+        if(method === 'GET') {
+            response.end('<h1>Halo! Ini adalah halaman about</h1>')
+        } else if(method === 'POST') {
+            let body = [];
     
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
-    
-    request.on('end', () => {
-        body = Buffer.concat(body).toString();
-        const { nama } = JSON.parse(body);
-        response.end(`<h1>Hai, ${nama}!</h1>`);
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+ 
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            });
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+        }
+    } else {
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
-    )};
-
-
-
-} 
-
-
-
-const server = http.createServer(requestListerner);
-
+};
+ 
+const server = http.createServer(requestListener);
+ 
 const port = 5000;
-const host = 'localhost'
-
-server.listen(port,host,()=>{
-console.log(`Server berjalan pada http://${host}:${port}`);   
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
 });
 
-
-/**
- * Panggil dengan cURL
- * 
- * curl -X GET http://localhost:5000
- * curl -X POST http://localhost:5000
- * curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"nama\": \"Ayana\"}"
- * dibaca nya di CMD/Terminal
- */
+// curl -X POST -H "Content-Type: application/json" http://localhost:5000/about -d "{\"name\": \"IHSAN\"}"
